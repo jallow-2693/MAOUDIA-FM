f77c6efaa7fa42f3b8a8b735b3a0543a2bca37fa
jallow-2693 <diallomaoudia13466@gmail.com>
2025-05-06 12:03:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,91 @@
     window.location.href = "login.html";
   });
 </script>
+// génération des joueurs 
+const postes = ["G", "DFC", "DFD", "DFG", "DLG", "DLD", "DLC", "MDD", "MDC", "MDG", "MOD", "MOG", "MOC", "ASC", "ASG", "ASD", "ATC", "ATD", "ATG"];
+const pieds = ["Droit", "Gauche", "bis"];
+const noms = ["Diallo", "Camara", "Traoré", "Sow", "Keita", "Cissé"];
+const prenoms = ["Oury", "Mamadou", "Aliou", "Ibrahima", "Cheick", "Lamine"];
+
+function randomItem(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function genererJoueur(id) {
+  let poste = randomItem(postes);
+  let talents = genererTalents(poste);
+  return {
+    nom: randomItem(noms),
+    prenom: randomItem(prenoms),
+    age: Math.floor(Math.random() * 26) + 15,
+    poste: poste,
+    pied_fort: randomItem(pieds),
+    note: Math.floor(Math.random() * 21) + 40,
+    condition: Math.floor(Math.random() * 31) + 70,
+    contrat: {
+      duree: Math.floor(Math.random() * 4) + 1,
+      salaire: Math.floor(Math.random() * 20000) + 30000,
+      statut: "Titulaire"
+    },
+    valeur: Math.floor(Math.random() * 800000) + 500000,
+    numero: id + 1,
+    club_actuel: "Haycom FC",
+    club_provenance: "Généré",
+    club_formateur: "Haycom FC",
+    talents: talents
+  };
+}
+
+function genererTalents(poste) {
+  const talentsDispo = {
+    G: ["Réflexes", "Détente", "Plongeon", "Relance", "Positionnement"],
+    DEF: ["Tacle", "Marquage", "Tête", "Endurance", "Vitesse"],
+    MIL: ["Passes", "Vision", "Tirs de loin", "Endurance", "Tacle", "Dribble"],
+    ATT: ["Finition", "Vitesse", "Tête", "Dribble", "Sang-froid"]
+  };
+
+  let talents = {};
+  let type;
+  if (poste === "G") type = "G";
+  else if (poste.startsWith("DF")) type = "DEF";
+  else if (poste.startsWith("M")) type = "MIL";
+  else type = "ATT";
+
+  talentsDispo[type].forEach(talent => {
+    talents[talent] = Math.floor(Math.random() * 41) + 60;
+  });
+  return talents;
+}
+
+function genererEquipe() {
+  const joueurs = [];
+  for (let i = 0; i < 16; i++) {
+    joueurs.push(genererJoueur(i));
+  }
+  localStorage.setItem("monEquipe", JSON.stringify(joueurs));
+  afficherJoueurs(joueurs);
+}
+
+function afficherJoueurs(joueurs = JSON.parse(localStorage.getItem("monEquipe") || "[]")) {
+  const tbody = document.querySelector("#tableJoueurs tbody");
+  tbody.innerHTML = "";
+  joueurs.forEach(joueur => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${joueur.prenom} ${joueur.nom}</td>
+      <td>${joueur.age}</td>
+      <td>${joueur.poste}</td>
+      <td>${joueur.pied_fort}</td>
+      <td>${joueur.note}</td>
+      <td>${joueur.condition}</td>
+      <td>${joueur.contrat.duree} ans</td>
+      <td>${joueur.valeur.toLocaleString()}</td>
+      <td>${joueur.numero}</td>
+      <td>${joueur.club_actuel}</td>
+    `;
+    tbody.appendChild(tr);
+  });
+}
+
+// Charger à l’ouverture si déjà généré
+window.onload = () => afficherJoueurs();
